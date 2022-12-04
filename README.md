190c077d80aa19d62375a88dd9f9ff1699cb655f
Brad Starkenberg <bstarke@Brads-Mac-mini.local>
2022-12-03 20:39:39 -0600

    update readme and staat on sync

--- README.md
+++ README.md
@@ -2,27 +2,32 @@
 
 ## Todo:
 
-- Convert to Cobra CLI
+- ~~Convert to Cobra CLI~~
 - Main Functions:
-  - daemon : run as background job (requires cron schedule in config)
+  - ~~csv : create csv of library~~
   - sync : one time sync (like daemon but doesn't sleep, quits after one run)
-  - csv : create csv of library
-  - print : print options to screen 
+  - daemon : run as background job (requires cron schedule in config)
+  - print : print options to screen
     - options:
       - movies (-t, --type movies)
-      - shows (-t, --type shows)
-      - only resolution not = config (-q, --quality)
+      - shows (-s, --type shows)
+      - only resolution not equal to config (-q, --quality)
       - only format not equal to config (-f, --format)
 
-## Sync Plan (daemon mode)
+## Sync Plan
 
-- Load DB for remote
 - Load DB for local
+- Load DB for remote
 - Fetch missing movies
+- Push missing movies
 - Fetch movies with better resolution
+- Push movies with better resolution
 - Fetch movies with preferred format
+- Push movies with preferred format
 - Fetch missing shows
-- Fetch shows with better resolution (if enabled)
+- Push missing shows
+- Fetch shows with better resolution
+- Push shows with better resolution
 
 ## Options
 
--- cmd/sync.go
+++ cmd/sync.go
@@ -18,20 +18,24 @@ limitations under the License.
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
 
+var movie_resolution, movie_format, show_resolution, show_format string
+
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Syncs the local plex server with the remote",
+	Long: `Sync will do a bi-directional sync with the remote
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This means that it will pull down all new movies from remote, 
+push new local movies to the remote, pull movies with a higher resolution,
+push movies if local resolution is higher, pull movies of the preferred format,
+push copy of formats not on the remote (assumes remote is what you want as master),
+it will then follow the same process for shows`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("sync called")
 	},
@@ -39,14 +43,20 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(syncCmd)
+	movie_resolution = viper.GetString("preferences.movies.resolution")
+	movie_format = viper.GetString("preferences.movies.format")
+	show_resolution = viper.GetString("preferences.shows.resolution")
+	show_format = viper.GetString("preferences.shows.format")
+}
 
-	// Here you will define your flags and configuration settings.
+func updateDatabaseForLocal() {
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// syncCmd.PersistentFlags().String("foo", "", "A help for foo")
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// syncCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func updateDatabaseForRemote() {
+
+}
+
+func fetchMissingMovies() {
+
 }
